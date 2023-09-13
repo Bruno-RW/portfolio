03b62e7ee4905004d974b982f770f97a903bf4d1
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-13 17:36:46 -0300

    Header mobile fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,11 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 import { links } from "@/lib/data";
 import { useMediaQuery } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/ActiveSectionContext";
-import { twMerge } from "tailwind-merge";
 
 const Header = () => {
   const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
@@ -16,11 +16,11 @@ const Header = () => {
   return (
     <header className="relative z-[999]">
       <motion.div
-        className="fixed -bottom-1 left-1/2 -translate-x-1/2 w-full h-[5rem] border border-white border-opacity-40 rounded-t-xl bg-white bg-opacity-80 backdrop-blur-[0.5rem] shadow-lg shadow-black/[0.03] sm:top-6 sm:w-[36rem] sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed -bottom-2 left-1/2 -translate-x-1/2 w-full h-[5rem] border border-white border-opacity-40 rounded-t-xl bg-white bg-opacity-80 backdrop-blur-[0.5rem] shadow-lg shadow-black/[0.03] sm:top-6 sm:w-[36rem] sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0,    x: "-50%", opacity: 1 }}
       />
-      <nav className={twMerge("flex fixed bottom-6 left-1/2 -translate-x-1/2 h-12 w-3/4 py-2 sm:top-[1.7rem] sm:w-auto sm:py-0", isMobileLg && "bottom-4")}>
+      <nav className={twMerge("flex fixed bottom-5 left-1/2 -translate-x-1/2 h-12 w-3/4 py-2 sm:top-[1.7rem] sm:w-auto sm:py-0", isMobileLg && "bottom-3")}>
         <ul className={twMerge("flex flex-wrap items-center justify-center text-[0.9rem] font-medium text-gray-500 w-full gap-y-2 gap-x-5 sm:w-[initial] sm:flex-nowrap sm:gap-5", isMobileLg && "justify-between")}>
           {links.map(link => (
             <motion.li className="flex relative items-center justify-center h-3/4"
