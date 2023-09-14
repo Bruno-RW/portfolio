ecca0138656dde4c6e464bd5e64e7342dab3df70
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-14 17:58:07 -0300

    Footer background aspect-ratio fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <>
-            <div className="w-full h-auto aspect-[900/200] bg-cover bg-center bg-no-repeat bg-[url('/img/background/footer-bg-light.svg')] dark:bg-[url('/img/background/footer-bg-dark.svg')]"></div>
+            <div className="w-full h-auto aspect-[900/400] bg-cover bg-center bg-no-repeat bg-[url('/img/background/footer-bg-light.svg')] sm:aspect-[900/200] dark:bg-[url('/img/background/footer-bg-dark.svg')]"></div>
 
             <footer className="relative text-center text-gray-300 pb-64 px-4 bg-[#0c5680] sm:pb-40 lg:pb-28 dark:text-gray-500 dark:bg-[#010911]">
                 <div className="absolute top-8 left-1/2 -translate-x-1/2 w-full px-3 lg:px-0">
