c05ca67f662634ac9a5391ae93b44729432a72f6
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-14 18:13:52 -0300

    Footer dark background color fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <>
             <div className="w-full h-auto aspect-[900/400] bg-cover bg-center bg-no-repeat bg-[url('/img/background/footer-bg-light.svg')] sm:aspect-[900/200] dark:bg-[url('/img/background/footer-bg-dark.svg')]"></div>
 
-            <footer className="relative text-center text-gray-300 pb-64 px-4 bg-[#0c5680] sm:pb-40 lg:pb-28 dark:text-gray-500 dark:bg-[#010911]">
+            <footer className="relative text-center text-gray-300 pb-64 px-4 bg-[#0c5680] sm:pb-40 lg:pb-28 dark:text-gray-400 dark:bg-[#044e51]">
                 <div className="absolute top-8 left-1/2 -translate-x-1/2 w-full px-3 lg:px-0">
                     <small className="text-xs block mb-2">
                         &copy; 2023 Bruno Rafael Wünsch. All rights reserved.
