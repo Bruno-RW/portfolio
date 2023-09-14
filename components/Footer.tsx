611ba61e78d13c8ce0ecba78598a9f1b39b54ac4
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-09-14 16:22:07 -0300

    Footer background styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,21 @@
-const Footer = () => {
-  return (
-      <footer className="text-center text-gray-500 px-4 mb-24 sm:mb-10">
-          <small className="text-xs block mb-2">
-              &copy; 2023 Bruno Rafael Wünsch. All rights reserved.
-          </small>
+"use client";
 
-          <p className="text-xs">
-              <span className="font-semibold">About this website: </span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel Hosting.
-          </p>
-      </footer>
-  );
+const Footer = () => {
+    return (
+        <>
+            <div className="w-full h-auto aspect-[900/200] bg-cover bg-center bg-no-repeat bg-[url('/img/background/footer-bg-light.svg')] dark:bg-[url('/img/background/footer-bg-dark.svg')]"></div>
+
+            <footer className="relative text-center text-gray-300 pb-64 px-4 bg-[#0c5680] sm:pb-40 lg:pb-28 dark:text-gray-500 dark:bg-[#010911]">
+                <div className="absolute top-8 left-1/2 -translate-x-1/2 w-full px-3 lg:px-0">
+                    <small className="text-xs block mb-2">
+                        &copy; 2023 Bruno Rafael Wünsch. All rights reserved.
+                    </small>
+                    <p className="w-full text-xs">
+                        <span className="font-semibold">About this website: </span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel Hosting.
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
 }
 export default Footer;
